cf8e222f79585130019d44cbbaf882088c49b66f
JL Reyes <joseluisreyes43@gmail.com>
2020-06-23 21:54:50 +0800

    added functions for add and connected to the button

--- basics/assets/scripts/app.js
+++ basics/assets/scripts/app.js
@@ -1,1 +1,11 @@
-alert('this works!');
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+function add(){
+    const calcDescription = `${currentResult} + ${userInput.value}`;
+    currentResult = currentResult + parseInt(userInput.value);
+    outputResult(currentResult, calcDescription);
+}
+
+addBtn.addEventListener('click', add);
+
