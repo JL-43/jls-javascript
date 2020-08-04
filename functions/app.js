178730874600de606eb75b1174fc87112d747f25
JL Reyes <joseluisreyes43@gmail.com>
2020-08-04 21:31:13 +0800

    finished the functions for the rockpaperscissors game, game is working

--- functions/app.js
+++ functions/app.js
@@ -4,6 +4,9 @@ const ROCK = 'ROCK';
 const PAPER = 'PAPER';
 const SCISSORS = 'SCISSORS';
 const DEFAULT_USER_CHOICE = 'ROCK';
+const RESULT_DRAW = 'DRAW';
+const RESULT_PLAYER_WINS = 'PLAYER WINS';
+const RESULT_COMPUTER_WINS = 'COMPUTER WINS';
 
 let gameIsRunning = false;
 
@@ -18,6 +21,39 @@ const getPlayerChoice = () => {
         return DEFAULT_USER_CHOICE;
     }
     return selection;
+};
+
+const getComputerChoice = () => {
+    const randomValue = Math.random();
+
+    // if(randomValue < 0.34) return ROCK;
+    // else if(randomValue < 0.67) return PAPER;
+    // else return SCISSORS;
+
+    return randomValue < 0.34 ? ROCK : randomValue < 0.67 ? PAPER : SCISSORS
+
+};
+
+const getWinner = (pChoice, cChoice) => {
+    // if(pChoice === cChoice){
+    //     return RESULT_DRAW;
+    // } else if (
+    //     pChoice === ROCK && cChoice === SCISSORS ||
+    //     pChoice === PAPER && cChoice === ROCK ||
+    //     pChoice === SCISSORS && cChoice === PAPER 
+    // ){
+    //     return RESULT_PLAYER_WINS;
+    // } else {
+    //     return RESULT_COMPUTER_WINS;
+    // }
+
+    return (pChoice === cChoice) ? RESULT_DRAW 
+    
+    : (pChoice === ROCK && cChoice === SCISSORS) ||
+    (pChoice === PAPER && cChoice === ROCK )||
+    (pChoice === SCISSORS && cChoice === PAPER) ? RESULT_PLAYER_WINS 
+    
+    : RESULT_COMPUTER_WINS
 }
 
 startGameBtn.addEventListener('click', function(){
@@ -26,6 +62,10 @@ startGameBtn.addEventListener('click', function(){
     }
     gameIsRunning = true;
     console.log('Game is starting');
-    const playerSelection = getPlayerChoice();
-    console.log(playerSelection);
+    const playerChoice = getPlayerChoice();
+    console.log(playerChoice);
+    const computerChoice = getComputerChoice();
+    console.log(computerChoice);
+    const winner = getWinner(playerChoice, computerChoice);
+    console.log(winner);
 });
