6ab88aac102589f23857e44572b246d6a900ded3
JL Reyes <joseluisreyes43@gmail.com>
2020-08-05 14:29:47 +0800

    finished the assignment

--- functions-assignment/assignment.js
+++ functions-assignment/assignment.js
@@ -1,5 +1,47 @@
-function sayHello(name) {
+// function sayHello(name) {
+//   console.log('Hi ' + name);
+// }
+
+const sayHello = (name) => {
   console.log('Hi ' + name);
 }
 
-sayHello();
+const sayHello2 = (greeting, name = 'JL') => {
+  console.log(greeting + ' ' + name);
+}
+
+const sayHello3 = () => {
+  console.log('Hi JL');
+}
+
+const sayHello4 = (name) => {
+  return 'Hi ' + name;
+}
+
+const checkInput = (cb, ...input) => {
+  let hasEmptyText= false;
+  for(const text of input){
+    if(!text){
+      hasEmptyText = true;
+      break;
+    }
+  }
+  if(!hasEmptyText){
+    cb();
+  }
+}
+
+sayHello('JL');
+sayHello2('Hey hey', 'JL');
+sayHello2('Hey');
+sayHello3();
+sayHello4();
+
+checkInput(
+  ()=>{
+    console.log('All not empty');
+  }, 
+  'Hello',
+  'Nice',
+  ''
+);
